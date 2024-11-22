541f41e95cf85b94edb3741bdac39152cc9c2c97
Moneexa <14besemsyed@seecs.edu.pk>
2024-11-22 12:15:34 +0100

    added wireframe for graphs

--- frontend/src/pages/Dashboard/Analytics/Analytics.tsx
+++ frontend/src/pages/Dashboard/Analytics/Analytics.tsx
@@ -3,7 +3,7 @@ import { Card, Col, Row } from 'antd';
 export function Analytics() {
   return (
     <>
-      <Row justify="center" gutter={10} style={{ margin: '24px 0' }}>
+      <Row justify="center" gutter={[10, 10]}>
         <Col span={12}>
           <Card title="Today's Food" bordered={true}>
             Card content
@@ -14,8 +14,6 @@ export function Analytics() {
             Card content
           </Card>
         </Col>
-      </Row>
-      <Row justify="center" gutter={10} style={{ margin: '24px 0' }}>
         <Col span={12}>
           <Card title="Historical Plan Cost" bordered={true}>
             Card content
@@ -26,13 +24,13 @@ export function Analytics() {
             Card content
           </Card>
         </Col>
-      </Row>
-      <Row justify="center" style={{ margin: '24px 0' }}>
-        <Card title="Your Remaining Food By Category">
-          <Col span={8}></Col>
-          <Col span={8}></Col>
-          <Col span={8}></Col>
-        </Card>
+        <Col span={12}>
+          <Card title="Your Remaining Food By Category">
+            <Col span={8}></Col>
+            <Col span={8}></Col>
+            <Col span={8}></Col>
+          </Card>
+        </Col>
       </Row>
     </>
   );
