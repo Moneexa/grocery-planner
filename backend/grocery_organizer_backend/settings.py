2a76817d394432a6d0dabb8049d494e7885a6149
Moneexa <14besemsyed@seecs.edu.pk>
2024-12-16 21:29:02 +0100

    => docker added for backend+frontend => added docker based vite config for server proxy

--- backend/grocery_organizer_backend/settings.py
+++ backend/grocery_organizer_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pykxs1e5pq*wv5080z97t**ea^57iaiow(u+6_e&zg=ea+qe4x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
